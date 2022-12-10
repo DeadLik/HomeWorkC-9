39826316196562d7bb6d5b6004ea228127ed39ac
Игорь <mc_grove@bk.ru>
2022-12-09 20:24:05 -0500

    add tasks

--- Task-2/Program.cs
+++ Task-2/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите два числа");
+Console.WriteLine();
+Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumNaturalElements(m, n, 0);
+
+void SumNaturalElements (int numM, int numN, int sum)
+{
+    if (numM > numN) 
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N = {sum}"); 
+        return;
+    }
+    sum += numM++;
+    SumNaturalElements(numM, numN, sum);
+}
--- Task-3/Program.cs
+++ Task-3/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int AckermanFunction(int numM, int numN)
+{
+    if (numM == 0) return numN + 1;
+
+    if (numM != 0 && numN == 0) return AckermanFunction(numM - 1, 1);
+
+    if (numM > 0 && numN > 0) return AckermanFunction(numM - 1, AckermanFunction(numM, numN - 1));
+
+    return AckermanFunction(numM, numN);
+}
+
+bool CheckNumbers(int numM, int numN)
+{
+    if (numM <= 0 || numN <= 0) return false;
+    return true;
+}
+
+// Решил модернизировать код
+
+while (true)
+{
+    Console.WriteLine("Введите два числа");
+    Console.WriteLine();
+    Console.Write("Введите число M: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число N: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    CheckNumbers(m, n);
+
+    if (CheckNumbers(m, n) == false)
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Введённые числа неположительные, M = {m}, N = {n}, повторите попытку");
+        Console.WriteLine();
+        Thread.Sleep(3000);
+        Console.Clear();
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Функция Аккермана для чисел A({m}, {n}) = {AckermanFunction(m, n)}");
+        break;
+    }
+}
